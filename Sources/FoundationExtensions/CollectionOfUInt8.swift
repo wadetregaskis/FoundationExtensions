8fa804808f935e5e7202347bc43e23d269114e98
Wade Tregaskis <wadetregaskis@mac.com>
2024-03-02 21:00:50 -0800

    Generalised asHexString to work on all Collections of UInt8s, not just Data.

---
+++ Sources/FoundationExtensions/CollectionOfUInt8.swift
@@ -0,0 +1,54 @@
+//  Created by Wade Tregaskis on 2024-03-02.
+
+extension Collection where Element == UInt8 {
+    func asHexString(uppercase: Bool = true,
+                     delimiterEvery: Int = 0,
+                     delimiter: String = " ") -> String {
+        let capacity = ((self.count * 2)
+                        + Swift.max(0,
+                                    ((0 != delimiterEvery)
+                                     ? (abs(self.count / delimiterEvery)
+                                        - ((0 == self.count % delimiterEvery)
+                                           ? 1
+                                           : 0)) * delimiter.utf8.count
+                                     : 0)))
+        let letterOffset = (uppercase ? UInt8(ascii: "A") : UInt8(ascii: "a")) - 10
+
+        return String(unsafeUninitializedCapacity: capacity) {
+            var i = 0
+            var nextSpaceIn = abs(delimiterEvery)
+
+            @inline(__always) func toHex(_ value: UInt8) -> UInt8 {
+                if value > 9 {
+                    return value + letterOffset
+                } else {
+                    return value + UInt8(ascii: "0")
+                }
+            }
+
+            for (offset, byte) in self.enumerated() {
+                $0[i] = toHex(byte / 16)
+                i += 1
+                $0[i] = toHex(byte % 16)
+                i += 1
+
+                if 0 != delimiterEvery && (offset + 1) < self.count {
+                    nextSpaceIn -= 1
+
+                    if 0 == nextSpaceIn {
+                        i = $0[i..<(i + delimiter.utf8.count)].initialize(fromContentsOf: delimiter.utf8)
+
+                        nextSpaceIn = abs(delimiterEvery)
+                    }
+                }
+            }
+
+            assert(i == capacity)
+            return i
+        }
+    }
+
+    var asHexString: String {
+        asHexString()
+    }
+}
--- Tests/FoundationExtensionsTests/DataTests.swift
+++ Tests/FoundationExtensionsTests/DataTests.swift
@@ -42,49 +42,4 @@ final class DataTests: XCTestCase {
             XCTAssertEqual(data.asString, testString)
         }
     }
-
-    func testAsHexString() throws {
-        let exemplarData = Data([11, 22, 33, 44, 55, 66, 0, 77, 88, 99, 111, 222, 255, 0])
-
-        for (testData, hexString) in [(Data(), ""),
-                                      (Data(repeating: 0, count: 3), "000000"),
-                                      (Data([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]), "000102030405060708090A0B0C0D0E0F10"),
-                                      (exemplarData, "0B16212C3742004D58636FDEFF00")] {
-            XCTAssertEqual(testData.asHexString(delimiterEvery: 0), hexString)
-            XCTAssertEqual(testData.asHexString, hexString)
-        }
-
-        for (spaceEvery, baseHexString) in ["0B 16 21 2C 37 42 00 4D 58 63 6F DE FF 00",
-                                        "0B16 212C 3742 004D 5863 6FDE FF00",
-                                        "0B1621 2C3742 004D58 636FDE FF00",
-                                        "0B16212C 3742004D 58636FDE FF00",
-                                        "0B16212C37 42004D5863 6FDEFF00",
-                                        "0B16212C3742 004D58636FDE FF00",
-                                        "0B16212C374200 4D58636FDEFF00",
-                                        "0B16212C3742004D 58636FDEFF00",
-                                        "0B16212C3742004D58 636FDEFF00",
-                                        "0B16212C3742004D5863 6FDEFF00",
-                                        "0B16212C3742004D58636F DEFF00",
-                                        "0B16212C3742004D58636FDE FF00",
-                                        "0B16212C3742004D58636FDEFF 00",
-                                        "0B16212C3742004D58636FDEFF00"].enumerated() {
-            // Ensure the defaults: uppercase, and delimiter is a single space character.
-            XCTAssertEqual(Data().asHexString(delimiterEvery: spaceEvery + 1), "")
-            XCTAssertEqual(Data().asHexString(delimiterEvery: -(spaceEvery + 1)), "")
-            XCTAssertEqual(exemplarData.asHexString(delimiterEvery: spaceEvery + 1), baseHexString)
-            XCTAssertEqual(exemplarData.asHexString(delimiterEvery: -(spaceEvery + 1)), baseHexString)
-            
-            for uppercase in [true, false] {
-                for delimiter in ["", " ", ":", " & ", "⭐"] {
-                    let adjustedDelimiterHexString = baseHexString.replacingOccurrences(of: " ", with: delimiter)
-                    let hexString = uppercase ? adjustedDelimiterHexString : adjustedDelimiterHexString.lowercased()
-                    
-                    XCTAssertEqual(Data().asHexString(uppercase: uppercase, delimiterEvery: spaceEvery + 1, delimiter: delimiter), "")
-                    XCTAssertEqual(Data().asHexString(uppercase: uppercase, delimiterEvery: -(spaceEvery + 1), delimiter: delimiter), "")
-                    XCTAssertEqual(exemplarData.asHexString(uppercase: uppercase, delimiterEvery: spaceEvery + 1, delimiter: delimiter), hexString)
-                    XCTAssertEqual(exemplarData.asHexString(uppercase: uppercase, delimiterEvery: -(spaceEvery + 1), delimiter: delimiter), hexString)
-                }
-            }
-        }
-    }
 }
